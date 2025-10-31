c9cbc5dd646f11f42695109b56d9d201e971b20b
yandersonpark1 <everydayap3@gmail.com>
2025-10-31 12:06:54 -0400

    updated submit script

--- submit.sh
+++ submit.sh
@@ -1,9 +1,19 @@
 #!/bin/bash 
 
-#SBATCH -p general 
+#SBATCH --job-name=proteinmpnn
+#SBATCH --output=/goldman_lab/mpnn/logs/pdb#.out
+#SBATCH --mem=16G
+#SBATCH --time=04:00:00
+#SBATCH --cpus-per-task=8
+#SBATCH --partition=standard
+#SBATCH --nodes=1
 
 source ~/.bashrc
 
 conda activate proteinmpnn
 
-python goldman_lab/software/ProteinMPNN/protein_mpnn_run.py
+python goldman_lab/software/ProteinMPNN/protein_mpnn_run.py  \
+    --pdb_path \
+    --out_folder \
+    --num_seq_per_target 10 \
+    --batch_size 1 \
